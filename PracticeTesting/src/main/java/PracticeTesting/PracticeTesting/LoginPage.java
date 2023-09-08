83c82415a27dd0adddeb697ffbcd894302a1fd53
cts15 <cos.narola@gmail.com>
2023-09-08 10:59:42 +0530

    firstcommit

---
+++ PracticeTesting/src/main/java/PracticeTesting/PracticeTesting/LoginPage.java
@@ -0,0 +1,34 @@
+package PracticeTesting.PracticeTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	WebDriver driver;
+	//constructor
+	public LoginPage(WebDriver driver)
+	{
+		super();
+		this.driver=driver;
+		//this.driver refers to local driver
+		PageFactory.initElements(driver, this); //to intialize elements using driver
+	}
+	//PageFactory design pattern
+		@FindBy(name="uid")
+		WebElement username;
+		
+		@FindBy(name="password")
+		WebElement passwordelement;
+		
+		@FindBy(name = "btnLogin")
+		WebElement login;
+		public void Login(String email,String password)
+		{
+			username.sendKeys(email);
+			passwordelement.sendKeys(password);
+			login.click();
+		}
+}
